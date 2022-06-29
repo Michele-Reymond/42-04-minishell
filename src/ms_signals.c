5fa2bd92b8412ea0c2b3803888a18ec52b58be09
EXO <exo@debian-BULLSEYE-live-builder-AMD64>
2022-06-29 11:10:38 +0200

    signal ctl-x et ctrl backslash)

--- src/ms_signals.c
+++ src/ms_signals.c
@@ -12,16 +12,44 @@
 
 #include "minishell.h"
 
-void sig_kill(int signum) {
-   printf("Caught signal %d, coming out...\n", signum);
-   return;
-//    exit(exit_status);
+void    on_sigquit(int sig, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	//printf ("signal quit ctrl backslash:%d\n",sig);
+	if (sig == 3)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
+void    on_siginit(int sig, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	//printf ("signal init ctrl_c:%d\n",sig);
+	if (sig == 2)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 //gestionnaire de signaux
 void    signal_handler(void)
 {
-    // signal(SIGINT, sig_kill);
-    // kill();
-    return ;
-}
+	struct sigaction	sia;
+
+	sigemptyset(&sia.sa_mask);
+	sia.sa_flags = SA_SIGINFO;
+	sia.sa_sigaction = on_sigquit;
+	sigaction(SIGQUIT, &sia, NULL);
+	sia.sa_sigaction = on_siginit;
+	sigaction(SIGINT, &sia, NULL);
+
+	return ;
